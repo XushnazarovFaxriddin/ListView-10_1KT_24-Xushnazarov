135f3020db2b7f95dc8db506e1690ca35bd0f860
Faxriddin Xushnazarov <coderxushnazarov@gmail.com>
2025-02-21 15:52:09 +0500

    update2

---
+++ app/src/main/java/com/example/listview_10_1kt_24_xushnazarov/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.listview_10_1kt_24_xushnazarov
+
+import android.os.Bundle
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    lateinit var textView: TextView
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        textView = findViewById(R.id.textView2)
+
+        // List yaratish
+        val fruits = listOf("Olma", "Banana", "Gilos", "Apelsin")
+        // Listni TextView'ga chiqarish
+        textView.text = fruits.joinToString("\n")
+    }
+}
